66d61fad5704056eeb7db83ff847505666c5717e
ts-roms <107532528+ts-roms@users.noreply.github.com>
2022-06-27 14:22:54 +0800

    - update on form

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -18,123 +18,152 @@ import {
   FormGroup,
   Stack,
   Button,
+  styled,
+  Paper,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import FolderIcon from "@mui/icons-material/Folder";
 
+const GridContainer = styled(Grid)(({ thene }) => ({
+  padding: "10px",
+}));
+
+const PaperContainer = styled(Paper)(({ theme }) => ({
+  padding: "25px",
+  marginTop: "14px",
+}));
+
+const products = [
+  {
+    category: "Goods",
+    name: "Product 1",
+    sizes: "m",
+    price: 11.0,
+    cost: 7.5,
+    inStock: 11,
+  },
+];
+
+const generateProducts = (products) => {
+  return products.map((product, key) => {
+    return (
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar>
+            <FolderIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={`Product name: ${product.name}`}
+          secondary={`Price: ${product.price}`}
+        />
+      </ListItem>
+    );
+  });
+};
+
 const Dashboard = () => {
   const dense = true; // useState
-  const secondary = true; // useState
 
   return (
     <React.Fragment>
       <Typography variant="h5" component="h1">
         Welcome to Utak.io
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Products
-          </Typography>
-          <List dense={dense}>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar>
-                  <FolderIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Single-line item"
-                secondary={secondary ? "Secondary text" : "Add"}
+      <PaperContainer elevation={3}>
+        <GridContainer container spacing={2}>
+          <Grid item md={6}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+              Products
+            </Typography>
+            <List dense={dense}>{generateProducts(products)}</List>
+          </Grid>
+          <Grid item md={6}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+              Product Entry Form
+            </Typography>
+            <FormControl fullWidth>
+              <InputLabel>Category</InputLabel>
+              <Select label="Category" value={10}>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField variant="outlined" label="Name" />
+            </FormControl>
+            <FormControl fullWidth>
+              <FormGroup>
+                <Stack spacing={2} direction="row">
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        defaultChecked
+                        inputProps={{ "aria-label": "controlled" }}
+                      />
+                    }
+                    label="Small"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        defaultChecked
+                        inputProps={{ "aria-label": "controlled" }}
+                      />
+                    }
+                    label="Medium"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        defaultChecked
+                        inputProps={{ "aria-label": "controlled" }}
+                      />
+                    }
+                    label="Large"
+                  />
+                </Stack>
+              </FormGroup>
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                variant="outlined"
+                label="Price"
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
               />
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Product Entry Form
-          </Typography>
-          <FormControl fullWidth>
-            <InputLabel>Category</InputLabel>
-            <Select label="Category" value={10}>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField variant="outlined" label="Name" />
-          </FormControl>
-          <FormControl fullWidth>
-            <FormGroup>
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                variant="outlined"
+                label="Cost"
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                variant="outlined"
+                label="In Stock"
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              />
+            </FormControl>
+            <FormControl fullWidth>
               <Stack spacing={2} direction="row">
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      defaultChecked
-                      inputProps={{ "aria-label": "controlled" }}
-                    />
-                  }
-                  label="Small"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      defaultChecked
-                      inputProps={{ "aria-label": "controlled" }}
-                    />
-                  }
-                  label="Medium"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      defaultChecked
-                      inputProps={{ "aria-label": "controlled" }}
-                    />
-                  }
-                  label="Large"
-                />
+                <Button variant="outlined" color="secondary">
+                  Clear
+                </Button>
+                <Button variant="contained">Submit</Button>
               </Stack>
-            </FormGroup>
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField
-              variant="outlined"
-              label="Price"
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField
-              variant="outlined"
-              label="Cost"
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField
-              variant="outlined"
-              label="In Stock"
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <Stack spacing={2} direction="row">
-              <Button variant="outlined" color="secondary">
-                Clear
-              </Button>
-              <Button variant="contained">Submit</Button>
-            </Stack>
-          </FormControl>
-        </Grid>
-      </Grid>
+            </FormControl>
+          </Grid>
+        </GridContainer>
+      </PaperContainer>
     </React.Fragment>
   );
 };
