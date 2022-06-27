cd13c75b8eca873f102f00556094e0afd134a82b
ts-roms <107532528+ts-roms@users.noreply.github.com>
2022-06-27 15:57:35 +0800

    - update form

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -15,11 +15,11 @@ import {
   InputLabel,
   Checkbox,
   FormControlLabel,
-  FormGroup,
   Stack,
   Button,
   styled,
   Paper,
+  Box,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import FolderIcon from "@mui/icons-material/Folder";
@@ -71,6 +71,10 @@ const generateProducts = (products) => {
 const Dashboard = () => {
   const dense = true; // useState
 
+  const handleSubmit = () => {
+    alert('Submitted')
+  };
+
   return (
     <React.Fragment>
       <Typography variant="h5" component="h1">
@@ -88,79 +92,112 @@ const Dashboard = () => {
             <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
               Product Entry Form
             </Typography>
-            <FormControl fullWidth>
-              <InputLabel>Category</InputLabel>
-              <Select label="Category" value={10}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl fullWidth>
-              <TextField variant="outlined" label="Name" />
-            </FormControl>
-            <FormControl fullWidth>
-              <FormGroup>
-                <Stack spacing={2} direction="row">
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        defaultChecked
-                        inputProps={{ "aria-label": "controlled" }}
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { mt: 2 },
+              }}
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+              <Grid container spaceing={2}>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <InputLabel>Category</InputLabel>
+                    <Select label="Category" value={10} required>
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <TextField variant="outlined" label="Name" required />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth sx={{ mt: 2 }}>
+                  <Typography variant="h6" component="label">
+                        Size
+                      </Typography>
+                    <Stack spacing={2} direction="row">
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            defaultChecked
+                            inputProps={{ "aria-label": "controlled" }}
+                          />
+                        }
+                        label="Small"
                       />
-                    }
-                    label="Small"
-                  />
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        defaultChecked
-                        inputProps={{ "aria-label": "controlled" }}
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            defaultChecked
+                            inputProps={{ "aria-label": "controlled" }}
+                          />
+                        }
+                        label="Medium"
                       />
-                    }
-                    label="Medium"
-                  />
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        defaultChecked
-                        inputProps={{ "aria-label": "controlled" }}
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            defaultChecked
+                            inputProps={{ "aria-label": "controlled" }}
+                          />
+                        }
+                        label="Large"
                       />
-                    }
-                    label="Large"
-                  />
-                </Stack>
-              </FormGroup>
-            </FormControl>
-            <FormControl fullWidth>
-              <TextField
-                variant="outlined"
-                label="Price"
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              />
-            </FormControl>
-            <FormControl fullWidth>
-              <TextField
-                variant="outlined"
-                label="Cost"
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              />
-            </FormControl>
-            <FormControl fullWidth>
-              <TextField
-                variant="outlined"
-                label="In Stock"
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              />
-            </FormControl>
-            <FormControl fullWidth>
-              <Stack spacing={2} direction="row">
-                <Button variant="outlined" color="secondary">
-                  Clear
-                </Button>
-                <Button variant="contained">Submit</Button>
-              </Stack>
-            </FormControl>
+                    </Stack>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <TextField
+                      variant="outlined"
+                      label="Price"
+                      inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                      type="number"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <TextField
+                      variant="outlined"
+                      label="Cost"
+                      inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                      type="number"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <TextField
+                      variant="outlined"
+                      label="In Stock"
+                      inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                      type="number"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+                    <Button variant="outlined" color="secondary" fullWidth>
+                      Clear
+                    </Button>
+                    <Button variant="contained" type="submit" fullWidth>
+                      Submit
+                    </Button>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Box>
           </Grid>
         </GridContainer>
       </PaperContainer>
